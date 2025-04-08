3002ec0a527f7bd5a329b372d062b6845cbad182
Craig Fisher <140781290+Firebro51@users.noreply.github.com>
2025-04-07 21:08:30 -0400

    Setting up plane controller/ Fixing mesh to be double sided

---
+++ Assets/Scripts/PlaneController.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class PlaneController : MonoBehaviour
+{
+
+    // Forces
+    // Rigidbody - Gravity- down
+    // drag - Slows down forward movement - back
+    // thrust - forward Speed - forward
+    // Lift - Upward force - up
+
+
+
+
+
+
+    // Rotation Variables
+    // Yaw, Roll, Pitch
+
+    // Roll - Forward axis  - x axis
+    // Yaw - Verticle axis  - y axis
+    // Pitch - Frontflip/Backflip - z axis
+
+
+
+
+    //
+
+    //
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
