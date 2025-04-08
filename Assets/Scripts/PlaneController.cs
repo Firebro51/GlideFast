5085276e5942898d8dcb0c60099854eff7c0efdd
Craig Fisher <140781290+Firebro51@users.noreply.github.com>
2025-04-08 00:10:14 -0400

    More plane controller setup

--- Assets/Scripts/PlaneController.cs
+++ Assets/Scripts/PlaneController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.XR;
 
 public class PlaneController : MonoBehaviour
 {
@@ -10,31 +11,51 @@ public class PlaneController : MonoBehaviour
     // Lift - Upward force - up
 
 
+    public float thrust = 20f;
+
+    public Rigidbody rb; // Gravity and drag
+
 
 
 
 
     // Rotation Variables
     // Yaw, Roll, Pitch
-
-    // Roll - Forward axis  - x axis
-    // Yaw - Verticle axis  - y axis
-    // Pitch - Frontflip/Backflip - z axis
-
-
-
-
-    //
-
-    //
+    
+    public float roll; // Roll - Forward axis  - x axis
+    public float yaw; // Yaw - Verticle axis  - y axis
+    public float pitch; // Pitch - Frontflip/Backflip - z axis
 
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
+    {
+        // Movement
+        HandleMovement();
+
+        // Look
+        HandleDirection();
+    }
+
+
+    // If hit ground die
+
+    // Have set starting speed in air
+
+
+    public void HandleMovement()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Add forward force to rb to see if moving forward works
+        }
+    }
+
+    public void HandleDirection()
     {
         
     }
